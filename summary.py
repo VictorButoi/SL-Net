3d07357710ba4953fe957734ffab97e4f74f7981
Victor Ion Butoi <vib9@cornell.edu>
2020-04-12 19:55:44 -0400

    pushing changes to model summary

--- summary.py
+++ summary.py
@@ -1,7 +1,9 @@
 from unet import UNet
 from unet import TiedUNet
 from torchsummary import summary
+import torch
 
-model = UNet(n_channels=3, n_classes=1, bilinear=True)
-#model = TiedUNet(n_channels=3, n_classes=1, bilinear=True)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#model = UNet(n_channels=3, n_classes=1, bilinear=True).to(device)
+model = TiedUNet(n_channels=3, n_classes=1, bilinear=True).to(device)
 summary(model, input_size=(3, 128, 191))
