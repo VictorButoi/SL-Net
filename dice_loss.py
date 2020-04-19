e25a2d24c62cc3bf5053cbb6fab09f7f252b819f
Victor Ion Butoi <vib9@cornell.edu>
2020-04-19 19:30:44 -0400

    fixing dice loss

--- dice_loss.py
+++ dice_loss.py
@@ -1,65 +1,8 @@
 import torch
 from torch.autograd import Function
 
-
-class DiceCoeff(Function):
-    """Dice coeff for individual examples"""
-
-    def forward(self, input, target):
-        self.save_for_backward(input, target)
-        eps = 0.0001
-        self.inter = torch.dot(input.view(-1), target.view(-1))
-        self.union = torch.sum(input) + torch.sum(target) + eps
-
-        t = (2 * self.inter.float() + eps) / self.union.float()
-        return t
-
-    # This function has only a single output, so it gets only one gradient
-    def backward(self, grad_output):
-
-        input, target = self.saved_variables
-        grad_input = grad_target = None
-
-        if self.needs_input_grad[0]:
-            grad_input = grad_output * 2 * (target * self.union - self.inter) \
-                         / (self.union * self.union)
-        if self.needs_input_grad[1]:
-            grad_target = None
-
-        return grad_input, grad_target
-
-
-def dice_coeff(input, target):
-    """Dice coeff for batches"""
-    if input.is_cuda:
-        s = torch.FloatTensor(1).cuda().zero_()
-    else:
-        s = torch.FloatTensor(1).zero_()
-
-    for i, c in enumerate(zip(input, target)):
-        s = s + DiceCoeff().forward(c[0], c[1])
-
-    return s / (i + 1)
-
-def multi_dice_loss(output, target):
-    """
-    output : NxCxHxW Variable
-    target :  NxHxW LongTensor
-    """
-    
-    eps = 0.0001
-
-    output = output.exp()
-    encoded_target = output.detach() * 0
-    encoded_target.scatter_(1, target.unsqueeze(1), 1)
-
-    weights = 1
-
-    intersection = output * encoded_target
-    numerator = 2 * intersection.sum(0).sum(1).sum(1)
-    denominator = output + encoded_target
-
-    denominator = denominator.sum(0).sum(1).sum(1) + eps
-    loss_per_channel = weights * (1 - (numerator / denominator))
-
-    return loss_per_channel.sum() / output.size(1)
+def dice_coeff(y_true, y_pred):
+    top = 2 * (y_true * y_pred, [1, 2, 3]).sum()
+    bottom = torch.max((y_true + y_pred, [1, 2, 3]).sum(), 50)
+    dice = torch.mean(top / bottom)
+    return -dice
