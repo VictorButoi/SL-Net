74a4e8a55a78421ba08e0c3e365c3694d65a695c
Victor Ion Butoi <vib9@cornell.edu>
2020-04-23 20:46:11 -0400

    left some old things in eval that needed to be removed

--- eval.py
+++ eval.py
@@ -16,27 +16,21 @@ def eval_net(net, loader, device, metric="DICE"):
     temp_boi2 = 0
 
 
-    #with tqdm(total=n_val, desc='Validation round', unit='batch', leave=False) as pbar:
-    for batch in loader:
-        imgs, true_masks = batch['image'], batch['mask']
-        imgs = imgs.to(device=device, dtype=torch.float32)
-        true_masks = true_masks.to(device=device, dtype=mask_type)
+    with tqdm(total=n_val, desc='Validation round', unit='batch', leave=False) as pbar:
+        for batch in loader:
+            imgs, true_masks = batch['image'], batch['mask']
+            imgs = imgs.to(device=device, dtype=torch.float32)
+            true_masks = true_masks.to(device=device, dtype=mask_type)
 
-        with torch.no_grad():
-            mask_pred = net(imgs) 
+            with torch.no_grad():
+                mask_pred = net(imgs) 
 
-        pred = mask_pred.argmax(1).unsqueeze(1)
+            pred = mask_pred.argmax(1).unsqueeze(1)
 
-        print(pred.cpu().numpy())
-        print(true_masks.cpu().numpy())
+            pred_dice = dice_coeff(pred, true_masks)[0].item()
 
-        pred_dice = dice_coeff(pred, true_masks)[0].item()
+            tot += pred_dice
 
-        tot += pred_dice
-        break
-
-        #pbar.update()
-
-    assert False, 'Breakpoint'
+            pbar.update()
 
     return tot / n_val
