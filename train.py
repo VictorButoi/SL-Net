85cbaa50826d0b4cce92dc6a89409e56da13e630
Victor Ion Butoi <vib9@cornell.edu>
2020-04-24 20:43:40 -0400

    pushing changss

--- train.py
+++ train.py
@@ -21,7 +21,8 @@ from dice_loss import one_hot
 
 dir_img = '/home/gid-dalcaav/projects/neuron/data/t1_mix/proc/resize256-crop_x32-slice100/train/vols/'
 dir_mask = '/home/gid-dalcaav/projects/neuron/data/t1_mix/proc/resize256-crop_x32-slice100/train/asegs/'
-dir_checkpoint = 'checkpoints/'
+dir_checkpoint_1 = 'checkpoints_1/'
+dir_checkpoint_2 = 'checkpoints_2/'
 
 np.set_printoptions(threshold=sys.maxsize)
 torch.set_printoptions(threshold=10_000)
@@ -34,7 +35,8 @@ def train_net(net,
               val_percent=0.1,
               save_cp=True,
               img_scale=1,
-              dice=True):
+              dice=True,
+              checkpoint=0):
 
     target_label_numbers = [0,2,3,4,10,16,17,28,31,41,42,43,49,53,63]
 
@@ -85,7 +87,7 @@ def train_net(net,
         with tqdm(total=n_train, desc=f'Epoch {epoch + 1}/{epochs}', unit='img') as pbar:
             for batch in train_loader:
                 net.train()
-                
+
                 imgs = batch['image']
                 true_masks = batch['mask']
                 assert imgs.shape[1] == net.n_channels, \
@@ -144,14 +146,22 @@ def train_net(net,
                         writer.add_images('masks/pred', torch.sigmoid(masks_pred) > 0.5, global_step)
 
         if save_cp:
-            try:
-                os.mkdir(dir_checkpoint)
-                logging.info('Created checkpoint directory')
-            except OSError:
-                pass
-            torch.save(net.state_dict(),
-                       dir_checkpoint + f'CP_epoch{epoch + 1}.pth')
-            logging.info(f'Checkpoint {epoch + 1} saved !')
+            if checkpoint == 1:
+                try:
+                    os.mkdir(dir_checkpoint_1)
+                    logging.info('Created checkpoint directory')
+                except OSError:
+                    pass
+                torch.save(net.state_dict(),dir_checkpoint_1 + f'CP_epoch{epoch + 1}.pth')
+                logging.info(f'Checkpoint {epoch + 1} saved !')
+            else:
+                try:
+                    os.mkdir(dir_checkpoint_2)
+                    logging.info('Created checkpoint directory')
+                except OSError:
+                    pass
+                torch.save(net.state_dict(),dir_checkpoint_2 + f'CP_epoch{epoch + 1}.pth')
+                logging.info(f'Checkpoint {epoch + 1} saved !')
 
     writer.close()
 
@@ -161,7 +171,7 @@ def train_net(net,
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target masks',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=5,
+    parser.add_argument('-e', '--epochs', metavar='E', type=int, default=3,
                         help='Number of epochs', dest='epochs')
     parser.add_argument('-b', '--batch-size', metavar='B', type=int, nargs='?', default=8,
                         help='Batch size', dest='batchsize')
@@ -189,27 +199,32 @@ if __name__ == '__main__':
     #   - For 1 class and background, use n_classes=1
     #   - For 2 classes, use n_classes=1
     #   - For N > 2 classes, use n_classes=N
-    net = UNet(n_channels=1, n_classes=15, bilinear=True)
-    #net = TiedUNet(n_channels=1, n_classes=15, bilinear=True)
+    net1 = UNet(n_channels=1, n_classes=15, bilinear=True)
+    net2 = TiedUNet(n_channels=1, n_classes=15, bilinear=True)
 
     logging.info(f'Network:\n'
                  f'\t{net.n_channels} input channels\n'
                  f'\t{net.n_classes} output channels (classes)\n'
                  f'\t{"Bilinear" if net.bilinear else "Transposed conv"} upscaling')
 
-    if args.load:
-        net.load_state_dict(
-            torch.load(args.load, map_location=device)
-        )
-        logging.info(f'Model loaded from {args.load}')
-
-    net.to(device=device)
+    net1.to(device=device)
+    net2.to(device=device)
     # faster convolutions, but more memory
 
-    train_scores, val_scores = train_net(net=net,
+    train_scores, val_scores = train_net(net=net1,
                                             epochs=args.epochs,
                                             batch_size=args.batchsize,
                                             lr=args.lr,
                                             device=device,
                                             img_scale=args.scale,
-                                            val_percent=args.val / 100)
+                                            val_percent=args.val / 100,
+                                            checkpoint=1)
+    
+    train_scores, val_scores = train_net(net=net2,
+                                            epochs=args.epochs,
+                                            batch_size=args.batchsize,
+                                            lr=args.lr,
+                                            device=device,
+                                            img_scale=args.scale,
+                                            val_percent=args.val / 100,
+                                            checkpoint=2)
