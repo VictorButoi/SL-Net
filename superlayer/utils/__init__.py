c3523024396c17dae2e4550dfd02c9ea89ff7786
Victor Ion Butoi <vib9@cornell.edu>
2020-05-21 15:04:48 -0400

    training works in the notebook again

--- superlayer/utils/__init__.py
+++ superlayer/utils/__init__.py
@@ -1,4 +1,5 @@
 from .dataset import BrainD
 from .dice_loss import dice_coeff
 from .dice_loss import one_hot
-from .helper import *
+from .helper import plot_img_array
+from .helper import plot_side_by_side
