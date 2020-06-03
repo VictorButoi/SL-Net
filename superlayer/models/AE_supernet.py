646078be0b0b1f04cb1c60fe87bddcd7ec961636
Victor Ion Butoi <vib9@cornell.edu>
2020-06-03 00:29:43 -0400

    testing transferability

--- superlayer/models/AE_supernet.py
+++ superlayer/models/AE_supernet.py
@@ -18,16 +18,15 @@ class AESuperNet(nn.Module):
         self.depth = depth
         self.n_classes = out_ch
         self.down = torch.nn.MaxPool2d(2,2)
-        half_size = int(superblock_size/2)
         
         #Kernel size is 3
         W = torch.nn.Parameter(torch.randn(superblock_size, superblock_size,3,3))
         W.requires_grad = True
 
-        self.block0 = simple_block(input_ch , half_size, use_bn)   
-        self.super_block = simple_block(superblock_size, half_size, use_bn, weight=W)
+        self.block0 = simple_block(input_ch , superblock_size, use_bn)   
+        self.super_block = simple_block(superblock_size, superblock_size, use_bn, weight=W)
 
-        self.out_conv = nn.Conv2d(half_size, out_ch, kernel_size=3, padding=1)
+        self.out_conv = nn.Conv2d(superblock_size, out_ch, kernel_size=3, padding=1)
         self.sm = nn.Softmax(dim=1)
 
         
