543407bc6eee25c8d6f2d5ca9c5cb4943e4abc74
Victor Ion Butoi <vib9@cornell.edu>
2020-10-25 05:34:02 -0400

    regular unet + training loop working, fixing sln

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,4 +1,3 @@
 from .validate import eval_net
 from .train import get_args
 from .train import train_net
-from .train_block import train_block
--- superlayer/utils/__init__.py
+++ superlayer/utils/__init__.py
@@ -1,5 +1,6 @@
 from .dataset import BrainD
-from .dice_loss import dice_coeff
+from .dice_loss import hard_dice
+from .dice_loss import soft_dice
 from .dice_loss import one_hot
 from .helper import plot_img_array
 from .helper import plot_side_by_side
